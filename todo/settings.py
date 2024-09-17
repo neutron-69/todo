9506721dc0f6be27d1dc93820a11e90dae720d2a
neutron-69 <mohul.batra@gmail.com>
2024-09-17 19:05:56 +0530

    changes

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--*h120u5l9)zj3xz4ei#c0w4-xt#yhpunzb)=c&v)st=t4iy4v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','todo-kski.onrender']
+ALLOWED_HOSTS = ['127.0.0.1','todo-kski.onrender.com']
 
 
 # Application definition
